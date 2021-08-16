66c3bf1766974d9c82c0de3601e61743e5940c60
Monicah Cloud <monicahcloud@gmail.com>
2021-08-15 22:15:24 -0400

    login matches Fiori's design

--- iLearn/src/app/services/user.service.ts
+++ iLearn/src/app/services/user.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import {Location} from '@angular/common';
 import {Router} from '@angular/router';
 import {User} from 'src/app/User';
+import {HttpClient} from '@angular/common/http';
+import { fromEventPattern, Observable, throwError } from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-
 
 export class UserService {
 
